268e9a0c19e4847e6dcf5d6870dc28a47b2a6372
Stan Leey <stanleey0@gmail.com>
2025-10-27 14:40:21 -0400

    1439 27102025 showing user the results of the scoring operation

--- src/web/index.mjs
+++ src/web/index.mjs
@@ -4,6 +4,8 @@ const scoreTextArea = document.getElementById("sentence-input");
 const scoreButton = document.getElementById("score-button");
 const hideButton = document.getElementById("hide-button");
 const resultsDiv = document.getElementById("scoring-results");
+const resultsInfoDiv = document.getElementById("results-info");
+const resultsDetailsDiv = document.getElementById("results-details");
 // Results are hidden
 hideResults();
 let resultsShown = false;
@@ -25,9 +27,17 @@ hideButton.addEventListener("click", () => {
 });
 
 function scoreGolfText(text) {
-  console.log("scoreGolfText called");
-  let score = scorer.score(text);
-  console.log("Result:", score);
+  // get the score from the other part of the program compiled with webpack
+  let score = scorer.score(text, { resultType: "detailed" });
+  // console.log(score);
+  resultsInfoDiv.innerText = `Total Score: ${score.totalScore}`;
+  // Clear and add the details
+  resultsDetailsDiv.innerHTML = "";
+  score.wordScores.forEach((wordScore) => {
+    resultsDetailsDiv.appendChild(makeWordScore(wordScore));
+  });
+  // Scroll to bottom so that the user can see the results
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function showResults() {
@@ -36,4 +46,20 @@ function showResults() {
 
 function hideResults() {
   resultsDiv.style.display = "none";
+}
+
+/**
+ * Makes a word score from the scoring item.
+ * @param {WordScore} param0 the word score object.
+ * @returns {HTMLSpanElement} the detailed word
+ */
+function makeWordScore({ word, wordScore, status }) {
+  let outerSpan = document.createElement("span");
+  outerSpan.classList.add("word-score-shell");
+  let scoreSpan = document.createElement("span");
+  scoreSpan.classList.add("word-score-window");
+  scoreSpan.innerText = wordScore;
+  outerSpan.innerText = word;
+  outerSpan.appendChild(scoreSpan);
+  return outerSpan;
 }
